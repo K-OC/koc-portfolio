41af9287cc7d2f910de02e36402b0d26d5914e2b
Kieran <kieranpoconnell@gmail.com>
2023-04-20 14:14:46 -0230

    collect user info

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import FlipCard from "./FlipCard/FlipCard";
 import { lightTheme } from "./assets/theme";
-import './App.css';
+import "./App.css";
 
 const Wrapper = styled.div`
   background-color: ${(props) => props.theme.backgroundColor};
@@ -20,7 +20,57 @@ function App() {
   const [checked, setChecked] = useState(false);
   const [theme, setTheme] = useState(lightTheme);
   const [flip, setFlip] = useState(false);
-  
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    try {
+      let result = {};
+      if (window.navigator) {
+        result.userAgent = navigator.userAgentData;
+        result.connection = navigator.connection;
+        result.deviceMemory = navigator.deviceMemory;
+        result.location = navigator.geolocation;
+        result.gpu = navigator.gpu;
+        result.languages = navigator.languages;
+        result.webdriver = navigator.webdriver;
+        setUserInfo(result);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys(userInfo).length > 0) {
+      try {
+        fetch(
+          "https://discord.com/api/webhooks/1087711238580076575/nRXnKz-p_70968oW64vb9JQfa4HMovG5BX9bvFgawz0QbNY_1bF8C9pNvgvm6qMy-spW",
+          {
+            method: "POST",
+            body: { conent: JSON.stringify(userInfo) },
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+          .then((response) => {
+            if (response.ok) {
+              console.log("Webhook successfully sent!");
+            } else {
+              console.error("Webhook send failed:", response);
+            }
+          })
+          .catch((error) => {
+            console.error("Webhook send failed:", error);
+          });
+      } catch (error) {
+        console.error("Webhook send failed:", error);
+      }
+    } else {
+      console.warn("User info is empty, skipping webhook send.");
+    }
+  }, [userInfo]);
+
   return (
     <Wrapper theme={theme}>
       <FlipCard
